e09f77ab467f6bab1b92eeab1a40f778fb8401e1
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-01-23 20:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,15 @@
     
 class Driver {
   constructor(name, day) {
-    this.name = name
-    this.day = day 
+    this.name = name;
+    this.day = day;
   }
 }
 
 class Route {
   constructor(beginningLocation, endingLocation) {
-    this.beginningLocation = beginningLocation,
-    this.endingLocation = endingLocation
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
   }
 }
 
